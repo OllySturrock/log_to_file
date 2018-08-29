56b86eaf47c0a5b3568c1fa1858b8a0ebbe83987
olly <olly.sturrock1@gmail.com>
2018-08-29 23:26:05 +0100

    spelling change

--- log_to_file.php
+++ log_to_file.php
@@ -1,6 +1,6 @@
 <?php
 function log_me($file_to_log_to, $what_to_log) {
-	//this only works for single line, after that the line is overwrit
+	//this only works for single line, after that the line is overwritten
 	$log_file = fopen($file_to_log_to, "w");
 	fwrite($log_file, $what_to_log);
 	fclose($log_file);
