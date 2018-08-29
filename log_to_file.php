52eb9b6eadc4042c549083a20be07389984a7d36
olly <olly.sturrock1@gmail.com>
2018-08-29 23:24:34 +0100

    initial

---
+++ log_to_file.php
@@ -0,0 +1,12 @@
+<?php
+function log_me($file_to_log_to, $what_to_log) {
+	//this only works for single line, after that the line is overwrit
+	$log_file = fopen($file_to_log_to, "w");
+	fwrite($log_file, $what_to_log);
+	fclose($log_file);
+}
+$log_me_test = false;
+if ($log_me_test == true) {
+	log_me("/var/www/html/global_backend/check.txt", "Success");
+}
+?>
